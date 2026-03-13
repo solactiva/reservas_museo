e28b42c5e17f765a4d332f947d986aab05ef03c8
solactiva <adeveloper236@gmail.com>
2026-03-13 09:36:51 -0400

    Update PagoRealizado.vue
    feature: Cambio de mensaje al registrar reserva

--- src/pages/PagoRealizado.vue
+++ src/pages/PagoRealizado.vue
@@ -2,14 +2,13 @@
 	<div class="text-center text-9xl my-16">🎉</div>
 	<div class="card">
 		<h3 class="text-3xl text-center mb-7">
-			Gracias, el pago de tu reserva está en proceso de verificación
+			Confirmación de Reserva en Proceso ⏳
 		</h3>
 		<p class="text-center">
-			Una vez confirmada tu reserva, enviaremos el detalle a tu correo
-			electrónico
+			Tras verificar tu pago, recibirás el detalle en tu email:
 			<span class="text-xl font-bold italic">{{ visitante.email }}</span
-			>. Si no recibes el correo electrónico, revisa tu carpeta de correo no
-			deseado. Tu reserva esta en proceso, para validar tu pago de forma segura contactanos al 
+			> (revisa también tu carpeta de spam). Si no registraste un correo o aún no recibes tu comprobante,
+			contáctanos de inmediato para validar tu pago y confirmar tu reserva al 
 			<a 
 				href="https://wa.me/59172877493?text=Hola%2C%20quiero%20validar%20mi%20reserva" 
 				target="_blank"
